68b419ebbc7b206585a3fb92050e8a25aa11c3fc
Theo <theo.piedade@gmail.com>
2023-08-12 22:56:43 -0300

    Api sign-up made

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import express from 'express'; // Importa o express da biblioteca
+import cors from 'cors';
 
 const app = express(); // Cria uma instância do servidor
+app.use(cors());
+app.use(express.json());
+
+const persons = [];
 
 // Configura uma função pra ser executada quando bater um GET na rota "/"
-app.get("/", (req, res) => {
-    // Manda como resposta o texto 'Hello World'
-    res.send('Hello World');
+app.post('/sign-up', (req, res) => {
+    const username = req.params.username;
+    const avatar = req.params.avatar;
+    if (username && avatar) persons.push()
+    res.send('OK');
+});
+
+app.post('/tweets', (req, res) => {
+    const username = req.params.username;
+    const avatar = req.params.avatar;
+    if (username && avatar) persons.push()
+    res.send('OK');
 });
 
 // Configura o servidor para rodar na porta 5000
