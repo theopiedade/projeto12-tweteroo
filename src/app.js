84733e928ba096cdc4626d2610790f61d1f6b1e0
Theo <theo.piedade@gmail.com>
2023-08-14 17:40:54 -0300

    get tweets with index check

--- src/app.js
+++ src/app.js
@@ -34,7 +34,8 @@ app.get('/tweets', (req, res) => {
     const arr = [];
     if (tweets.length > 0)
     tweets.map((tweet, i) => {
-        let avatar = persons.find((person) => person.username === tweet.username);
+        let avatarIndex = persons.username.indexOf(tweet.username);
+        let avatar = persons.avatar[avatarIndex];
         if (arr.length < 9)
          arr.push({ username: tweet.username, avatar: avatar, tweet: tweet.tweet})
      } )
